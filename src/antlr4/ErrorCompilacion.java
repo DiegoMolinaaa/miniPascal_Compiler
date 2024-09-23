5846007760ee16dd17e970e6e076707fc886900d
pamelagiselle8 <pamelaramirez@unitec.edu>
2024-09-22 20:59:27 -0600

    FileChooser para seleccionar programa

--- src/antlr4/ErrorCompilacion.java
+++ src/antlr4/ErrorCompilacion.java
@@ -3,14 +3,14 @@ package antlr4;
 public class ErrorCompilacion {
     int fila;
     int columna;
-    String instruccion;
+//    String instruccion;
     String descripcion;
     ErrorTipo tipo;
 
-    public ErrorCompilacion(int fila, int columna, String instruccion, String descripcion, ErrorTipo tipo) {
+    public ErrorCompilacion(int fila, int columna, String descripcion, ErrorTipo tipo) {
         this.fila = fila;
         this.columna = columna;
-        this.instruccion = instruccion;
+//        this.instruccion = instruccion;
         this.descripcion = descripcion;
         this.tipo = tipo;
     }
@@ -26,7 +26,7 @@ public class ErrorCompilacion {
                 "Error " + tipo + ":\n" +
                 "Fila " + fila +
                 ", columna " + columna +
-                "\nInstrucción: '" + instruccion + '\'' +
+//                "\nInstrucción: '" + instruccion + '\'' +
                 "\nDescripción: '" + descripcion + '\'' + '\n' ;
     }
 }
